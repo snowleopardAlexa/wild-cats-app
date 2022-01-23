245ce5480403bcccdf7ac730e3fd337321d9513d
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-23 12:57:11 -0500

    add react router v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/Homepage/HomePage';
+import Types from './components/Types/Types';
+import RandomCat from './components/RandomCat/RandomCat';
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Footer />
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="types/*" element={<Types />} />
+        <Route path="random/*" element={<RandomCat />} />
+      </Routes>
+       <Footer />
+     </BrowserRouter>
     </div>
   );
 }
