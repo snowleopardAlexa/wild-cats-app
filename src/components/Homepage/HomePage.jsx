2684fdf0504011249fc6a665fe478d2802bd0c3a
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-23 14:11:36 -0500

    add search term with use state to homepage

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react';
 import './homepage.scss';
 
 const HomePage = () => {
-  return (
-    <div className="home">
 
+  const [searchTerm, setSearchTerm] = useState("");  
+
+  return (
+    <div className="homepage">
+      <div className="homepage-search">
+         <input type="text" 
+            placeholder="Search a cat..." 
+            value={searchTerm} 
+            onChange={(e) => setSearchTerm(e.target.value)} />
+         <button>Search Cat</button>
+      </div>
+      <div className="homepage-cats">
+
+      </div>
     </div>
   );
 };
