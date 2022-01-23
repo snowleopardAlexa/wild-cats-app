d669947c7c4d8edc37c199275de0b3c27340be29
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-23 18:37:53 -0500

    add fuse.js

--- api/data.js
+++ api/data.js
@@ -2,8 +2,8 @@ module.exports = (req, res) => {
     if (req.method === 'GET') {
         res.json ([
            {name: "Lion", habitant: "Africa", left: 3000, level: "Endangered"},
-           {name: "Lion", habitant: "Africa", left: 3000, level: "Endangered"},
-           {name: "Lion", habitant: "Africa", left: 3000, level: "Endangered"},
+           {name: "Tiger", habitant: "Asia", left: 2000, level: "Endangered"},
+           {name: "Snow Leopard", habitant: "Asia", left: 8000, level: "Endangered"},
         ])
     } else {
         const {name, habitant, left, level} = req.body;
