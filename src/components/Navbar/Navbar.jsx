57b85bc7a73ec1793454c3fe17f9e85808b3687d
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-23 12:34:29 -0500

    create navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 const Navbar = () => {
   return (
     <div className="navbar">
-
-    </div>
+      <div className="navbar-heading">
+        <h1>Wild Cats<span>App</span></h1>
+      </div>  
+        <div className="navbar-links">
+          <ul>
+            <li>Home</li>
+            <li>Cats Types</li>
+            <li>Random</li>
+          </ul>
+        </div>
+      </div>
   );
 };
 
